9a74b4335d2d296ee6bad29fad04d553acadd5cc
yenhsi <cynthiahsieh218@gmail.com>
2021-04-13 16:05:58 +0800

    race

---
+++ src/main/java/cc/racing/HorseRunnable.java
@@ -0,0 +1,10 @@
+package cc.racing;
+
+public class HorseRunnable implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("RR " +  i);
+        }
+    }
+}
