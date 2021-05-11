a3f8885433d88ffc83879d5b3bb655311c69482f
yenhsi <cynthiahsieh218@gmail.com>
2021-05-11 15:21:39 +0800

    Treeset

--- src/main/java/com/collection/Tester.java
+++ src/main/java/com/collection/Tester.java
@@ -3,12 +3,30 @@ package com.collection;
 import com.test.Box;
 import com.test.Box1;
 import com.test.Box2;
+import cc.stud.Student;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
 
 public class Tester {
     public static void main(String[] args) {
-        int[] nums = new int[5];
+        TreeSet<String> aa = new TreeSet<>();
+        System.out.println(aa.size());
+        aa.add("Cynthia");
+        aa.add("Jerry");
+        aa.add("Tom");
+        aa.add("Cynthia");
+        System.out.println(aa.size());
+        System.out.println(aa);
+        for (String a : aa) {
+            System.out.println(a);
+        }
+
+
+        }
+
+/*        int[] nums = new int[5];
         nums[0] = 3;
 //        nums[3] = "Cynthia";
 //        nums[2] = true;
@@ -19,8 +37,8 @@ public class Tester {
         Box1 box = new Box1();
         list.add(box);
         // Heterogeneous NG!?
-        String name = (String)list.get(0);
-        String kk = (String)list.get(1);
+        String name = (String) list.get(0);
+        String kk = (String) list.get(1);
         //
         Box1 box1 = new Box1();
         Box2 box2 = new Box2();
@@ -29,5 +47,5 @@ public class Tester {
         boxes.add(box2);
         Box b = boxes.get(0);
 
-    }
+    }*/
 }
