235d9d7eb71db8d5d183d986c9d3b19ca9355e93
yenhsi <cynthiahsieh218@gmail.com>
2021-06-01 14:58:59 +0800

    CarWin

---
+++ src/main/java/cc/car/CarWin.java
@@ -0,0 +1,14 @@
+package cc.car;
+
+import javax.swing.*;
+
+public class CarWin {
+    static final int WIDTH = 500;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH, 450);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
