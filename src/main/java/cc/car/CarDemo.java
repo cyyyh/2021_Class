011b6c7a828bf22f9288ba81528e54bcd58beafc
yenhsi <cynthiahsieh218@gmail.com>
2021-06-01 16:45:34 +0800

    CarWin2.2

--- src/main/java/cc/car/CarDemo.java
+++ src/main/java/cc/car/CarDemo.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 public class CarDemo {
     private JPanel mainPanel;
-    private JTextField textField1;
     private JPanel northPanel;
+    private JTextField textField1;
     private JButton button1;
 
     public static void main(String[] args) {
